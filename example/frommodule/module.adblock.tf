e3602d287f3fca1ed665f70d88b38df3eaa381e9
James Woolfenden <james.woolfenden@gmail.com>
2023-04-18 15:03:44 +0100

    another example

---
+++ example/frommodule/module.adblock.tf
@@ -0,0 +1,5 @@
+module "adblock" {
+ source        = "JamesWoolfenden/adblock/cloudflare"
+  version      = "0.0.2"
+  team_name    = "woolfenden"
+}
---
+++ example/frommodule/terraform.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "3.33.1"
+    }
+  }
+  backend "s3" {
+    profile        = "personal"
+    encrypt        = true
+    bucket         = "680235478471-terraform-state"
+    key            = "cloudflare/team/terraform.tfstate"
+    dynamodb_table = "dynamodb-state-lock"
+    region         = "eu-west-2"
+  }
+}
